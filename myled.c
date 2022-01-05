e9c470d013e659bb5fc79c8cace35709765fc4cd
MatsuiYamato <74082029+MatsuiYamato@users.noreply.github.com>
2022-01-05 21:48:27 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (C) 2022 MatsuiYamato.  All rights reserved.
+ * Copyright (C) 2022 MatsuiYamato and Ryuichi Ueda.  All rights reserved.
  */
 
 #include <linux/module.h>
